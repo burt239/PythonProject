0366e5f1fbd0d7017bfdb7334762107a4a866cc4
burtovskayaeo.29 <e.burtovskaya@gmail.com>
2026-03-05 18:19:29 +0300

    фон, белка двигается кнопками, есть пасхалко и кол, если 1 очко, окно пасхалко

--- otchislenie.py
+++ otchislenie.py
@@ -1,26 +1,32 @@
 import pygame
 import random
+
+from pygame.examples.go_over_there import screen
+
+pygame.font.init()
 window=pygame.display.set_mode((1280,823))
-class person:
-    x=0
-    y=0
-    img = ''
-
-    def __init__(self,x_pers,y_pers,way):
-        self.x = x_pers
-        self.y = y_pers
-        self.img = pygame.image.load(way)
-
-    
+x = 50
+y = 50
+x2 = (1240-150)//2
+y2 = (700-210)//2
+xkol = x2
+ykol = y2
+count = 0
+fx = True
+fy = True
+cont = False
 bg=pygame.image.load('petya1.jpg')
-pashalko=person(0,0,'239.png')
-belka=person(50,50,'otch.png')
+pashalko=pygame.image.load('239.png')
+belka=pygame.image.load('otch.png')
+kol = pygame.image.load('kol tozhe ozenka.png')
+bgifpas = pygame.image.load('фон если пасхалко.png')
+bott = pygame.image.load('continue bottom.png')
 
 k = random.randint(0, 700)
-speed = random.randint(10, 100)
+speed = 0.5
 while 1:
     window.blit(bg,(0,0))
-
+    window.blit(pygame.font.SysFont("Arial",100).render('очки: '+str(count),0,(0,0,0),None),(10,600))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
@@ -63,23 +69,72 @@ while 1:
     else:
         y1=y
 
-    print(x1,y1, "    ",x,y)
+
 
     window.blit(belka, (x, y))
     window.blit(belka, (x1, y1))
 
-
-    if x2<1280-150:
-       x2=x2+(speed//10)
+    if fx:
+        xkol=xkol+(speed)
+        if xkol>=1240-32:
+            fx=False
     else:
-        d=0-(speed//10)
-        y2 = random.randint(0, 700)
-        speed = random.randint(10, 100)
+        xkol=xkol-(speed)
+        if xkol<=0:
+            fx=True
+
+    if fy:
+        ykol=ykol+(speed)
+        if ykol>=700-76:
+            fy=False
+    else:
+        ykol=ykol-(speed)
+        if ykol<=0:
+            fy=True
+
+
+
+
 
     window.blit(pashalko, (x2, y2))
+    window.blit(kol, (xkol, ykol))
+
+
+    if (pygame.mask.from_surface(pashalko).overlap(pygame.mask.from_surface(belka), ( x - x2, y-y2))) or (pygame.mask.from_surface(pashalko).overlap(pygame.mask.from_surface(belka), ( x1 - x2, y1-y2))):
+        count += 1
+        y2 = random.randint(0, 700 - 150)
+        x2 = random.randint(0, 1240 - 210)
+
+    if (pygame.mask.from_surface(kol).overlap(pygame.mask.from_surface(belka), (x - xkol, y - ykol))) or (pygame.mask.from_surface(kol).overlap(pygame.mask.from_surface(belka), ( x1 - xkol, y1-ykol))):
+        count -= 239
+        speed += 0.5
+        ykol = random.randint(0, 700 - 76)
+        xkol = random.randint(0, 1240 - 32)
+
+    if count == 1:
+        cont = True
+        while cont:
+            x = 50
+            y = 50
+            x2 = (1240 - 150) // 2
+            y2 = (700 - 210) // 2
+            xkol = x2
+            ykol = y2
+            count = 0
+            fx = True
+            fy = True
+            window.blit(bgifpas,(0,0))
+            window.blit(pygame.font.SysFont("Arial", 60).render('67', 0, (0, 0, 0), None), (150, 420))
+            window.blit(bott, ((1280-360)//2, (823-270)//2))
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                        xb, yb = event.pos
+                        if (xb<1280//2) and (xb>1280//2-360) and (yb > 823//2-270) and (yb < 823//2):
+                            cont = False
+            pygame.display.update()
+        count -= 1
+    count += 1
+
     pygame.display.update()
-
-    if pygame.mask.from_surface(pashalko).overlap(pygame.mask.from_surface(belka), ( x - p2.x, )):
-
 
 
